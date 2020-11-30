d41c101e0103817d0acd277a4f459b1d5e8ec2fa
jurgen1c <jurgen1c@gmail.com>
2020-11-29 22:05:58 -0600

    Add get & erase defs

--- lists.py
+++ lists.py
@@ -29,6 +29,31 @@ class linked_list:
       current_node = current_node.next
       nodes.append(current_node.data)
     print(nodes) 
+  
+  def get(self, index):
+    if index >= self.length():
+      print("Error: 'Get' Index out of range!")
+      return None
+    current_index = 0
+    current_node = self.head
+    while True:
+      current_node = current_node.next
+      if current_index == index: return current_node.data
+      current_index += 1
+
+  def erase(self, index):
+    if index >= self.length():
+      print("ERROR: 'Erase' Index out of range!")
+      return
+    current_index = 0
+    current_node = self.head
+    while True:
+      last_node = current_node
+      current_node = current_node.next
+      if current_index == index:
+        last_node.next = current_node.next
+        return
+      current_index += 1
 
 my_list = linked_list()
 
@@ -36,7 +61,14 @@ my_list.display()
 
 my_list.append(1)
 my_list.append(2)
+my_list.append(3)
+my_list.append(4)
 
 my_list.display()
 print(my_list.length())
+
+print (my_list.get(2))
+
+my_list.erase(1)
+my_list.display()
 
