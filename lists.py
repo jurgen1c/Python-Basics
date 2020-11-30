f97f6fb7c9a7bf3f614a4a1f8a3d484edb2f6458
jurgen1c <jurgen1c@gmail.com>
2020-11-29 21:53:40 -0600

    Add append, length & display defs

--- lists.py
+++ lists.py
@@ -7,4 +7,36 @@ class linked_list:
   def __init__(self):
     self.head = node()
   
+  def append(self, data):
+    new_node = node(data)
+    current = self.head
+    while current.next != None:
+      current = current.next
+    current.next = new_node
   
+  def length(self):
+    current = self.head
+    total = 0
+    while current.next != None:
+      total += 1
+      current = current.next
+    return total
+
+  def display(self):
+    nodes = []
+    current_node = self.head
+    while current_node.next != None:
+      current_node = current_node.next
+      nodes.append(current_node.data)
+    print(nodes) 
+
+my_list = linked_list()
+
+my_list.display()
+
+my_list.append(1)
+my_list.append(2)
+
+my_list.display()
+print(my_list.length())
+
